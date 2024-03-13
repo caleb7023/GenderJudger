7e3971c3b6ce8134d809e8b286d42fd3c8589968
caleb7023 <caleb70230@gmail.com>
2024-03-13 16:44:22 +0900

    wrote readme

--- ProcessImg.py
+++ ProcessImg.py
@@ -3,12 +3,14 @@ import cv2
 import numpy as np
 
 def main():
-    for index, dir in os.listdir("man/"):
+    for index, dir in enumerate(os.listdir("man/")):
         Img = cv2.imread(f"man/{dir}")
+        cv2.resize(Img, (100, 100))
         np.save(f"Data/Man/{index}", Img)
 
-    for index, dir in os.listdir("woman/"):
+    for index, dir in enumerate(os.listdir("woman/")):
         Img = cv2.imread(f"woman/{dir}")
+        cv2.resize(Img, (100, 100))
         np.save(f"Data/Woman/{index}", Img)
 
 
