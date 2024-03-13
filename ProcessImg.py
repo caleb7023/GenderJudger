b83105d4252e6e189503e5b38bf8686bf7fe7ba7
caleb7023 <caleb70230@gmail.com>
2024-03-13 16:43:04 +0900

    done ig

--- ProcessImg.py
+++ ProcessImg.py
@@ -1,9 +1,16 @@
 import os
 import cv2
-import cupy
+import numpy as np
 
 def main():
-    for 
+    for index, dir in os.listdir("man/"):
+        Img = cv2.imread(f"man/{dir}")
+        np.save(f"Data/Man/{index}", Img)
+
+    for index, dir in os.listdir("woman/"):
+        Img = cv2.imread(f"woman/{dir}")
+        np.save(f"Data/Woman/{index}", Img)
+
 
 if __name__ == "__main__":
     main()
