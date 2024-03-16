a8ba04c3a2295ac0e02001e75286c3e4ab633f71
caleb7023 <caleb70230@gmail.com>
2024-03-16 16:19:15 +0900

    Data will be loaded from the ram now

--- Train.py
+++ Train.py
@@ -2,42 +2,35 @@
 
 # Author: caleb7023
 
-import numpy as np   # For loading
+import numpy as cp   # For loading
 import os            # To get the list of the data files
 import time
 if False: import cv2 # For debugging
 
 # Load the neuron weights
-NeuronWeights = np.load("Data/NeuronWeights.npy")
+NeuronWeights = cp.load("Data/NeuronWeights.npy")
 
 # Train the AI
-def Train(Dir : str, Man : bool):
+def Train(Data, Man : bool):
 
     global NeuronWeights
 
-    # Load the img to train the AI
-    Img = np.load(Dir)
-
     if False: # Debug mode
-        cv2.imshow("debug", np.uint8(Img)) # show the loaded img
-        cv2.imshow("debug2", np.uint8(NeuronWeights)) # show the neuron weights
+        cv2.imshow("debug", cp.uint8(Img).get()) # show the loaded img
+        cv2.imshow("debug2", cp.uint8(NeuronWeights).get()) # show the neuron weights
         cv2.waitKey(1)
 
     # Check the gender prediction is right or not.
     # If prediction wasnt right -> Modify the neuron weights.
     # If prediction was right -> Do nothin.
-    if (0 < np.sum(Img * NeuronWeights)) != Man:
-        if Man: NeuronWeights += Img
-        else  : NeuronWeights -= Img
+    if (0 < cp.sum(Data * NeuronWeights)) != Man:
+        if Man: NeuronWeights += Data
+        else  : NeuronWeights -= Data
         return 1
     return 0
 
 
 def main():
-
-    # Count how much file is in the man and woman face data directory.
-    ManFileCount   = len(os.listdir("Data/man"))
-    WomanFileCount = len(os.listdir("Data/woman"))
 
     global NeuronWeights
 
@@ -51,9 +44,9 @@ def main():
 
         StartTime = time.time()
 
-        for i in range(10000):
-            Fails += Train(f"Data/man/{int(Terms*.5 % ManFileCount)}.npy"    , True)
-            Fails += Train(f"Data/woman/{int(Terms*.5 % WomanFileCount)}.npy", False)
+        for i in range(20000):
+            Fails += Train(Datas[0][int(Terms*.5 %  ManFileCount )], True)
+            Fails += Train(Datas[1][int(Terms*.5 % WomanFileCount)], False)
             Terms += 2
         
         TotalFails += Fails
@@ -61,11 +54,27 @@ def main():
         # save the neuron weights, terms and total fails.
         with open("Data/Terms"     , "w") as f: f.write(str(Terms))
         with open("Data/TotalFails", "w") as f: f.write(str(TotalFails))
-        np.save("Data/NeuronWeights", NeuronWeights)
+        cp.save("Data/NeuronWeights", NeuronWeights)
 
         # Print the data to console to check train progress
-        print(f"Total terms:{Terms}, Total fails:{TotalFails}, Fails:{Fails}/20000, Time:{round(time.time() - StartTime, 3)}")
+        print(f"Total terms:{Terms}, Total fails:{TotalFails}, Fails:{Fails}/40000, Time:{round(time.time() - StartTime, 3)}")
 
+
+def LoadDatas():
+
+    global Datas
+
+    for i in range( ManFileCount ): Datas[0] += [cp.load( f"./Data/man/{i}.npy" )]
+    for i in range(WomanFileCount): Datas[1] += [cp.load(f"./Data/woman/{i}.npy")]
 
 if __name__ == "__main__":
+    
+    # Count how much file is in the man and woman face data directory.
+    ManFileCount   = len(os.listdir("./Data/man"))
+    WomanFileCount = len(os.listdir("./Data/woman"))
+
+    Datas = [[], []]
+
+    LoadDatas()
+
     main()
